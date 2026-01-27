1176b9d313ba8f9963d5587e6218d6e5d6e4a3d6
LunaticLeo <liaoyufu.leo@gmail.com>
2026-01-27 00:12:10 -0500

    sync

--- 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
+++ 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,12 +1,8 @@
 package test_java_eclipse;
 
-import java.util.ArrayList;
-
 public class test {
-    public static void main(String[] args) {
-    	
-    	ArrayList<String> a = new ArrayList();
-    	a.add("123");
-    	System.out.print(a.isEmpty());
-    }
+	
+	public static void main(String[] args) {
+		System.out.print( () -> Math.random() );
+	}
 }
