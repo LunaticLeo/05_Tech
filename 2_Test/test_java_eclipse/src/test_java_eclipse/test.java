49e99d0a62ae8fad12058cc7e99ebca4ca684986
LunaticLeo <liaoyufu.leo@gmail.com>
2026-01-27 23:43:29 -0500

    sync

--- 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
+++ 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,8 +1,14 @@
 package test_java_eclipse;
 
+import java.util.function.Function;
+
 public class test {
-	
-	public static void main(String[] args) {
-		System.out.print( () -> Math.random() );
-	}
+    
+    public static void main(String[] args) {
+
+    	Integer i = 100;
+    	Boolean b = false;
+    	System.out.println(Integer.compare(1, 0));
+
+    }
 }
