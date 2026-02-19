3864b79879aeed7cf2122b7e19a86ae68f641e93
LunaticLeo <liaoyufu.leo@gmail.com>
2026-02-18 23:07:45 -0500

    sync

--- 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
+++ 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,62 +1,27 @@
 package test_java_eclipse;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.function.Function;
-
-public class test {
+public class test {		
 	
-	public static List<List<Integer>> solution(int[] nums){
+	public static int solution(int[] input, int kth) {
+			
+		int point = input.length/2;
 		
-		HashMap<Integer, HashSet<String>> hm = new HashMap<>();
-		
-		for(int i=0;i<nums.length;i++) {
-			for(int j=i+1;j<nums.length;j++) {
-				HashSet<String> temp = hm.getOrDefault(nums[i]+nums[j], new HashSet<String>());
-				if(nums[i]<nums[j]) {
-					temp.add(""+nums[i]+","+nums[j]);
-				}else {
-					temp.add(""+nums[j]+","+nums[i]);
-				}
-				hm.put(nums[i]+nums[j], temp);
-			}
-		}
-				
-		HashSet<Integer> numsUnique = new HashSet<>();
-		List<List<Integer>> output = new ArrayList<>();
-		
-		for(int i=0;i<nums.length;i++) {
-			if(numsUnique.contains(nums[i]) == false) {
-				numsUnique.add(nums[i]);
-				if(hm.containsKey(0-nums[i])) {
-					HashSet<String> temp = hm.get(0-nums[i]);
-										
-					for(String s: temp) {
-						ArrayList<Integer> temp2 = new ArrayList<Integer>();
-						String[] parts = s.split(",");
-						temp2.add(nums[i]);
-						temp2.add(Integer.parseInt(parts[0]));
-						temp2.add(Integer.parseInt(parts[1]));
-						output.add(temp2);
+		while(point == kth) {
+			int a = 0, b = input.length-1;
+			for(int i =0; i<input.length;i++) {
+				if(i != point) {
+					if(input[i]>input[point]) {
+						
 					}
-					
 				}
 			}
 		}
 		
-		System.out.println(output);
-		
-		return output;
+		return 0;
 	}
-	
     
     public static void main(String[] args) {
     	
-    	solution(new int[] {0,0,0});
-    	solution(new int[] {-1,0,1,2,-1,-4});
-    	solution(new int[] {0,1,1});
-
+    	
     }
 }
