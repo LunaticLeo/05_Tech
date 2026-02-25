bd9ce9a2cf2f49f003bcb27a4975f85e247cf5e7
LunaticLeo <liaoyufu.leo@gmail.com>
2026-02-25 00:19:18 -0500

    sync

--- 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
+++ 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
@@ -6,31 +6,21 @@ public class test {
 
 	public static int solution(int[] nums) {
 
-		Map<Integer, Integer> map = new HashMap<>();
-	    int max = 0;
+		return null;
 
-	    for (int num : nums) {
-	        if (!map.containsKey(num)) {
-	            int left = map.getOrDefault(num - 1, 0);
-	            int right = map.getOrDefault(num + 1, 0);
-
-	            int sum = left + 1 + right;
-
-	            map.put(num, sum);
-	            max = Math.max(max, sum);
-	            
-	            map.put(num - left, sum);
-	            map.put(num + right, sum);
-	        }
-	    }
-	    System.out.println(max);
-	    return max;
 	}
 
 	public static void main(String[] args) {
 
-		solution(new int[] { 100, 4, 200, 1, 3, 2 });
-		solution(new int[] { -1, 0, 1, 2, -1, -4 });
-		solution(new int[] { 0, 1, 1 });
+//		solution(new int[] { 100, 4, 200, 1, 3, 2 });
+//		solution(new int[] { -1, 0, 1, 2, -1, -4 });
+//		solution(new int[] { 0, 1, 1 });
+		
+		ArrayList<Integer> a = new ArrayList<>(List.of(1,2,3));
+		
+		a.addAll(1,List.of(4));
+		
+		System.out.print(a);
+		
 	}
 }
