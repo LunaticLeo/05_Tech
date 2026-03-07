aef3c5cf2407fdfe35c42d19d19881a966f35959
LunaticLeo <liaoyufu.leo@gmail.com>
2026-03-06 23:12:24 -0500

    sync

--- 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
+++ 2_Test/test_java_eclipse/src/test_java_eclipse/test.java
@@ -6,21 +6,15 @@ public class test {
 
 	public static int solution(int[] nums) {
 
-		return null;
+		return 0;
 
 	}
 
 	public static void main(String[] args) {
 
-//		solution(new int[] { 100, 4, 200, 1, 3, 2 });
-//		solution(new int[] { -1, 0, 1, 2, -1, -4 });
-//		solution(new int[] { 0, 1, 1 });
+
 		
-		ArrayList<Integer> a = new ArrayList<>(List.of(1,2,3));
 		
-		a.addAll(1,List.of(4));
-		
-		System.out.print(a);
 		
 	}
 }
