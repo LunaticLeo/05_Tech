34bd2c443d4d6b623d2787c4ca33b7fc7d57f78e
LunaticLeo <liaoyufu.leo@gmail.com>
2026-08-02 14:27:25 +0800

    sync

---
+++ 2_Test/test_cpp_cmake/test_cpp_cmake.cpp
@@ -0,0 +1,12 @@
+﻿// test_cpp_cmake.cpp : Defines the entry point for the application.
+//
+
+#include "test_cpp_cmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
